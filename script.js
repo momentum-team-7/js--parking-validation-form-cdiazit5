7df45c8deabb8ba47f2aac98f0d72c9abbdaa0ba
cdiazit5 <cdiazit5@gmail.com>
2021-02-02 08:34:21 -0500

    step 3 on assignment restart

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('Add validation!');
+const form = document.getElementById('parking-form')
+const carYear = document.querySelectorAll('car-year')
+const numberDays = document.getElementById('days')
+
+
+// form.addEventListener('submit', (event) => {
+//     function ()
+
+// });
+// // Car year must be after 1900
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    alert('SUBMITTED');
+});
